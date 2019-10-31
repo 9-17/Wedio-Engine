76420987ea1bb15f03884f00fcec636f6d61e6b9
kls6378 <kls6378@naver.com>
2019-10-31 10:27:02 +0900

    Change all to PUG

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ const app = express()
 const PORT = 9562
 
 // 템플릿(퍼그) 세팅
-app.set('views','views')
-app.set('view engine','pug')
+app.set('views', 'views')
+app.set('view engine', 'pug')
 
 // 정적파일 로드를 위해 
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(PORT, ()=>{
+app.get('/list', (req, res) => {
+    res.render('list')
+})
+
+app.get('/upload', (req, res) => {
+    res.render('upload')
+})
+
+app.get('/playing', (req, res) => {
+    res.render('playing')
+})
+
+app.listen(PORT, () => {
     console.log(`Running on ${PORT}!!`)
 })
